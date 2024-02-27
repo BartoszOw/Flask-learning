284cfb641c146c5ffadca75601b8e8077ea835ec
BartoszOw <owbartosz.pl@gmail.com>
2024-02-27 15:06:06 +0100

    Changing sites

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/mypage/me')
 def page_me():
-    return render_template('first_page.html')
+    site = "contact"
+    return render_template('first_page.html', site=site)
 
 @app.route('/mypage/contact')
 def contact_page():
-    return render_template('sec_page.html')
+    site = "me"
+    return render_template('sec_page.html', site=site)
