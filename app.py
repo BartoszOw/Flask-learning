22779c3ce1223c96cacc4181781465bb6e295812
BartoszOw <owbartosz.pl@gmail.com>
2024-02-27 15:54:51 +0100

    POST method done

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ def page_me():
     site = "contact"
     return render_template('first_page.html', site=site)
 
-@app.route('/mypage/contact')
+@app.route('/mypage/contact', methods=["GET", "POST"])
 def contact_page():
     site = "me"
-    return render_template('sec_page.html', site=site)
+    if request.method == "POST": 
+        print(request.form)
+        return redirect('/mypage/contact')
+    elif request.method == "GET": 
+        return render_template('sec_page.html', site=site)
+    
+
+if __name__ == '__main__':
+    app.run()
+    
+    
